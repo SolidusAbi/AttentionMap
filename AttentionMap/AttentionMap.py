333154e798b5d3a30db421cb22b71a1b759d03d7
Abián Hernández <abianhernandez@outlook.es>
2022-01-17 17:09:18 +0000

    Fixing errors in AttentionMap module

--- AttentionMap/AttentionMap.py
+++ AttentionMap/AttentionMap.py
@@ -5,9 +5,10 @@ from Sparse import SparseSigmoid
 
 class AttentionMap(nn.Module):
     def __init__(self, in_features: int) -> None:
+        super(AttentionMap, self).__init__()
         self.attention_map = nn.Sequential(*[
             nn.Conv2d(in_channels=in_features, out_channels=1, kernel_size=1, padding=0, bias=False),
-            nn.SparseSigmoid(beta=1e-6, rho=0.05)
+            SparseSigmoid(beta=1e-6, rho=0.05)
         ]) 
 
     def forward(self, x: Tensor) -> Tensor:
@@ -15,10 +16,11 @@ class AttentionMap(nn.Module):
 
 class Conv2d(nn.Module):
     def __init__(self, in_channels, out_channels, **args) -> None:
+        super(Conv2d, self).__init__()
         self.attention_map = AttentionMap(in_features=in_channels)
         self.conv = nn.Conv2d(in_channels, out_channels, **args)
 
     def forward(self, x: Tensor) -> Tensor:
-        x = self.conv(x)
         att = self.attention_map(x)
+        x = self.conv(x)
         return att*x
