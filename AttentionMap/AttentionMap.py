f32126721be0156abdb3f8f60fe39831fd857840
Abián Hernández <abianhernandez@outlook.es>
2022-01-20 12:47:05 +0000

    More tests...

--- AttentionMap/AttentionMap.py
+++ AttentionMap/AttentionMap.py
@@ -4,25 +4,30 @@ from torch import nn
 from Sparse import SparseSigmoid, SparseSigmoid2d, ReLUWithSparsity2d
 from modules.Sparse.Sparse.modules.activations import ReLUWithSparsity
 
+from torch.nn import functional as F
+
 class AttentionMap(nn.Module):
-    def __init__(self, in_features: int, out_features: int) -> None:
+    def __init__(self, in_features: int) -> None:
         super(AttentionMap, self).__init__()
         self.attention_map = nn.Sequential(*[
             nn.Conv2d(in_channels=in_features, out_channels=1, kernel_size=1, padding=0, bias=False),
-            SparseSigmoid(beta=1e-6, rho=0.05)
-            # ReLUWithSparsity(beta=1e-6, rho=0.05)
+            # SparseSigmoid(beta=1e-6, rho=0.1)
+            ReLUWithSparsity(beta=1e-6, rho=0.15),
+            # nn.ReLU(inplace=True)
         ]) 
 
     def forward(self, x: Tensor) -> Tensor:
-        return clip(self.attention_map(x), max=1)
+        # att = self.attention_map(x)
+        # N, _, W, H = att.shape
+        # return F.softmax(att.view(N,1,-1), dim=2).view(N,1,W,H)
+        return self.attention_map(x)
 
 class Conv2d(nn.Module):
     def __init__(self, in_channels, out_channels, **args) -> None:
         super(Conv2d, self).__init__()
-        self.attention_map = AttentionMap(in_features=in_channels, out_features=out_channels)
+        self.attention_map = AttentionMap(in_features=in_channels)
         self.conv = nn.Conv2d(in_channels, out_channels, **args)
 
     def forward(self, x: Tensor) -> Tensor:
         att = self.attention_map(x)
-        x = self.conv(x)
-        return att*x
+        return att*self.conv(x)
