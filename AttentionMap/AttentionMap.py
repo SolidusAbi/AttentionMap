c60db0fbf931a55b98c164a54e9b2a96af9d7f18
Abián Hernández <abianhernandez@outlook.es>
2022-07-03 17:59:48 +0100

    Minor tests using VGG-16

--- AttentionMap/AttentionMap.py
+++ AttentionMap/AttentionMap.py
@@ -3,14 +3,14 @@ import torch
 from torch import Tensor, clip
 from torch import nn
 
-from Sparse import SparseSigmoid, SparseSigmoid2d, ReLUWithSparsity2d
-from modules.Sparse.Sparse.modules.activations import ReLUWithSparsity
-
+# from Sparse import SparseSigmoid, SparseSigmoid2d, ReLUWithSparsity2d
 from torch.nn import functional as F
 
 class AttentionMap(nn.Module):
     def __init__(self, in_features: int) -> None:
+        from modules.Sparse.Sparse.modules.activations import ReLUWithSparsity
         super(AttentionMap, self).__init__()
+        
         self.attention_map = nn.Sequential(*[
             nn.Conv2d(in_channels=in_features, out_channels=1, kernel_size=1, padding=0, bias=False),
             # SparseSigmoid(beta=1e-6, rho=0.1)
